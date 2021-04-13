7c6ff8c30b9d6ea42cfe8b123a84ebf0b3556299
Shimona <74822423+Shimona012@users.noreply.github.com>
2021-04-13 12:37:31 +0530

    Add files via upload

---
+++ level3.js
@@ -0,0 +1,17 @@
+function level3(){
+    console.log("level 3");
+    background(bg2);
+    bb2.visible=false;
+    bb3.visible=false;
+    bbees.visible=false;
+    Minu.visible=false;
+    redG.destroyEach();
+    pinkG.destroyEach();
+    blueG.destroyEach();
+    yellowG.destroyEach();
+    bb2.velocityX=0;
+    bb3.velocityX=0;
+    bbees.velocityX=0;
+    textSize(25);
+    text("LEVEL 3",400,200);
+}
